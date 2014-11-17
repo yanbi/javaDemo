916b3cebb55552773c8c08e84e088d0b57a7b0d7
yanbi <yanbi@fzi.de>
2014-11-17 17:42:34 +0100

    new javaDemo

---
+++ javaDemo/test/de/fzi/javaDemo/test/Function1Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.fzi.javaDemo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author yanbi
+ *
+ */
+public class Function1Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

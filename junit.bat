d7b6b749798b2196a33a3c8bf3926cd20f8a000e
yanbi <yan.bi@fzi.de>
2014-11-19 02:43:04 +0100

    add susessc junit.bat

--- junit.bat
+++ junit.bat
@@ -1,7 +1,10 @@
-mkdir testJunit
-cd testJunit
-xcopy Z:\EclipseWorkspace\git\javaDemo /e
+rd bin /S /Q
+mkdir bin
+:: rd testJunit /S /Q
+:: mkdir testJunit
+:: xcopy javaDemo testJunit /E
 javac -cp lib\junit4.11.jar -d bin test\de\fzi\javaDemo\test\Function1Test.java
-java -cp .;lib\junit4.11.jar;lib\hamcrest1.3.jar;bin\de\fzi\javaDemo\test; org.junit.runner.JUnitCore Function1Test
+
+java -cp .;lib\junit4.11.jar;lib\hamcrest1.3.jar;bin; org.junit.runner.JUnitCore de.fzi.javaDemo.test.Function1Test
 
 pause

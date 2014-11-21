7ac07d3d9863fb92b1094df8ae766b97bc4a05cd
yanbi <yanbi@fzi.de>
2014-11-21 17:36:52 +0100

    update junit.bat

--- junit.bat
+++ junit.bat
@@ -3,8 +3,9 @@ mkdir bin
 :: rd testJunit /S /Q
 :: mkdir testJunit
 :: xcopy javaDemo testJunit /E
-javac -cp lib\junit4.11.jar -d bin test\de\fzi\javaDemo\test\Function1Test.java
-
-java -cp .;lib\junit4.11.jar;lib\hamcrest1.3.jar;bin; org.junit.runner.JUnitCore de.fzi.javaDemo.test.Function1Test
-
+javac -cp lib\junit4.11.jar -d bin test\de\fzi\javaDemo\test\*.java
+::javac -cp lib\junit4.11.jar -d bin test\de\fzi\javaDemo\test\Function1Test.java
+cd bin\de\fzi\javaDemo\test
+java -cp .;..\..\..\..\..\lib\junit4.11.jar;..\..\..\..\..\lib\hamcrest1.3.jar;..\..\..\..\..\bin; org.junit.runner.JUnitCore *
+::java -cp .;lib\junit4.11.jar;lib\hamcrest1.3.jar;bin; org.junit.runner.JUnitCore de.fzi.javaDemo.test.Function1Test
 pause
---
+++ test/de/fzi/javaDemo/test/Function2Test.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package de.fzi.javaDemo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author yanbi
+ *
+ */
+public class Function2Test {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+
+}
